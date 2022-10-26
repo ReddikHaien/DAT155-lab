880f9da27ee3165c7c12cf5aa59c4ab86645134a
ReddikHaien <584969@stud.hvl.no>
2022-10-26 12:25:51 +0200

    added update method

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,4 @@
-import { WebGLRenderer } from "three";
+import { Color, WebGLRenderer } from "three";
 
 export default class App{
 
@@ -8,12 +8,16 @@ export default class App{
         this.renderer = new WebGLRenderer({
             canvas: document.createElement("canvas")
         });
-
         this.renderer.setSize(window.innerWidth,window.innerHeight);
-
         document.body.appendChild(this.renderer.domElement);
-
         window.onresize = this.resize.bind(this);
+    
+        this.renderer.setClearColor(new Color(0.0,1.0,0.0));
+        this.renderer.setAnimationLoop(this.update.bind(this));
+    }
+
+    update(){
+        this.renderer.clear();
     }
 
     resize(){
